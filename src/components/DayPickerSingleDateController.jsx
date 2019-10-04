a0555c7396de72be3c46fb4ea5668b26c3e71e73
Minh Nguyen <minhnguyenxx@gmail.com>
2019-10-04 01:58:50 +0100

    Rename unsafe lifecycles

--- src/components/DayPickerSingleDateController.jsx
+++ src/components/DayPickerSingleDateController.jsx
@@ -199,7 +199,7 @@ export default class DayPickerSingleDateController extends React.PureComponent {
     this.isTouchDevice = isTouchDevice();
   }
 
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     const {
       date,
       focused,
@@ -333,7 +333,7 @@ export default class DayPickerSingleDateController extends React.PureComponent {
     }
   }
 
-  componentWillUpdate() {
+  UNSAFE_componentWillUpdate() {
     this.today = moment();
   }
 
